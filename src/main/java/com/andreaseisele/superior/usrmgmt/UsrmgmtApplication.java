b1e1e9f8943ec6d97215f1cc8f98fd2263bb01bd
Andreas Eisele <ae@andreaseisele.com>
2019-01-22 23:28:02 +0100

    initial commit

---
+++ src/main/java/com/andreaseisele/superior/usrmgmt/UsrmgmtApplication.java
@@ -0,0 +1,14 @@
+package com.andreaseisele.superior.usrmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsrmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsrmgmtApplication.class, args);
+	}
+
+}
+
