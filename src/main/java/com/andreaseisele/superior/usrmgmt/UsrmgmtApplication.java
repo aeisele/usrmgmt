b747b6326718abc2eea56128826c4752a792616c
marco <marco@marcobehler.com>
2019-02-17 16:12:08 +0100

    My main method

--- src/main/java/com/andreaseisele/superior/usrmgmt/UsrmgmtApplication.java
+++ src/main/java/com/andreaseisele/superior/usrmgmt/UsrmgmtApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsrmgmtApplication {
 
 	public static void main(String[] args) {
+		System.out.println("luulluul");
 		SpringApplication.run(UsrmgmtApplication.class, args);
 	}
 
